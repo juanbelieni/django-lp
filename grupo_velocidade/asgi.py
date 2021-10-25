6378e1fac55e9c248cea60708e11a4de50b8e152
adamesalles <adamesalles111@gmail.com>
2021-10-25 08:20:35 -0300

    Adding django proj and req

---
+++ grupo_velocidade/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for grupo_velocidade project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grupo_velocidade.settings')
+
+application = get_asgi_application()
