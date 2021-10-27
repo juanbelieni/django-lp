ebfacdb3cc74f9709f9ba9bb5395bf6af93894d6
GustavoSanches55 <gsanchescosta@yahoo.com.br>
2021-10-27 09:30:52 -0300

    Adicionando página

--- instagram2/views.py
+++ instagram2/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('<h1>IG2</h1>')
+    
+def perfil(request):
+    return render(request, "intagram2/index.html")
