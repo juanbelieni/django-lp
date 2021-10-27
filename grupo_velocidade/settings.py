d6a0d667dabe5a51cbbe5f5fec733c981367c720
adamesalles <adamesalles111@gmail.com>
2021-10-27 09:08:07 -0300

    Adicionando página do template

--- grupo_velocidade/settings.py
+++ grupo_velocidade/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'grupo_velocidade.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
