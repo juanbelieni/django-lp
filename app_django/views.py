00c34ac8fd1266c951d0cf734d18e24f838999a5
Nicole dos Santos <nicoledossantosouza@gmail.com>
2021-10-29 08:16:28 -0300

    Create page app_index.html in app_django

--- app_django/views.py
+++ app_django/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>Um belo exemplo</h1>")
+
+def app_index(request):
+    return render(request, 'app_django/app_index.html')
