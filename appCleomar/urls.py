4a8bcfbff0d38c9c5b29a1096e26c379a204db68
cleomarfelipe <cleomarfelipe@gmail.com>
2021-10-29 08:37:29 -0400

    Add template appCleo/elogio.html

--- appCleomar/urls.py
+++ appCleomar/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 import appCleomar.views as appCleoViews
 
 urlpatterns = [
-    path('', appCleoViews.index),
+    path('', appCleoViews.index, name='index'),
+    path('elogio/', appCleoViews.elogio, name='elogio')
 ]
