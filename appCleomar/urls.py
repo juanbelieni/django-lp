6bb45ac79e6d0eca6bc6f33957a6d1328003ce45
cleomarfelipe <cleomarfelipe@gmail.com>
2021-11-05 07:53:36 -0400

    debug appCleomar

--- appCleomar/urls.py
+++ appCleomar/urls.py
@@ -3,6 +3,6 @@ import appCleomar.views as appCleoViews
 
 urlpatterns = [
     path('', appCleoViews.index, name='index'),
-    path('elogio/', appCleoViews.elogio, name='elogio')
+    path('elogio/', appCleoViews.elogio, name='elogio'),
     path('todo/<int:id>', appCleoViews.todo, name='todo-dinamico')
 ]
