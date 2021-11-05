49a5a39bd4cd623c9cbff4690d9e79b65c3dae51
adamesalles <adamesalles111@gmail.com>
2021-11-05 08:38:27 -0300

    Adicionando http-redirect e url dinâmica no wpp2

--- whatsapp_2/urls.py
+++ whatsapp_2/urls.py
@@ -4,4 +4,6 @@ import whatsapp_2.views as wpp2_views
 urlpatterns = [
     path('', wpp2_views.index, name='index'),
     path('contacts/', wpp2_views.contacts, name='contacts'),
+    path('profile/', wpp2_views.redirect_to_contacts, name='profile-redirect'),
+    path('profile/<str:param>', wpp2_views.profile, name='profile')
 ]
