3860688aa1751439b1c28e6b2db4bdca3a512a43
Juan Belieni <juanbelieni@gmail.com>
2021-11-05 08:38:26 -0300

    Redirecionamento para o ZapZap 2™

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,7 @@ import todo.views as todo_views
 
 urlpatterns = [
     path('', todo_views.index),
+    path('zapzap2/', todo_views.zapzap2),
     path('todo/', todo_views.todos),
+    path('todo/<int:id>', todo_views.todo, name="todo-dinamico")
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
@@ -6,7 +6,15 @@ from django.shortcuts import render
 def index(request):
     return HttpResponse("<h1>TODO APP</h1>")
 
+# Redirect to '/whatsapp2/'
+def zapzap2(request):
+    return HttpResponseRedirect('/whatsapp2')
 
 # Return all todos
 def todos(request):
     return render(request, "todo/index.html")
+
+
+# Create dynamic view
+def todo(request, id):
+    return HttpResponse(f"<h1>TODO {id}</h1>")
