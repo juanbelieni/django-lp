02ac36fbe7f12a8c6ef96847fc82a069b8e130ab
Juan Belieni <juanbelieni@gmail.com>
2021-11-08 08:41:49 -0300

    "Nada é tão horrível que não possa piorar muito!"

--- todo/urls.py
+++ todo/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', todo_views.index),
     path('zapzap2/', todo_views.zapzap2),
     path('todo/', todo_views.todos),
-    path('todo/<int:id>', todo_views.todo, name="todo-dinamico")
+    path('todo/<int:id>', todo_views.todo, name="todo-dinamico"),
+    path('todo_2', todo_views.todo_2, name="todo-template"),
+    path('animes/', todo_views.animes),
 ]
--- todo/views.py
+++ todo/views.py
@@ -14,7 +14,20 @@ def zapzap2(request):
 def todos(request):
     return render(request, "todo/index.html")
 
-
 # Create dynamic view
 def todo(request, id):
     return HttpResponse(f"<h1>TODO {id}</h1>")
+
+# Create view with 'todo-2.html' template
+def todo_2(request):
+    data = { 'name': 'Mr. LinkedIn' }
+    return render(request, "todo/todo-2.html", data)
+
+def animes(request):
+    animes = [
+        'Neon Genesis Evangelion',
+        'Cowboy Bebop',
+        'Death Note',
+        'Full Metal Alchemist',
+    ]
+    return render(request, "todo/filtros.html", { 'animes': animes })
