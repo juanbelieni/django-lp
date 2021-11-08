a528d7a3cf2c5e866bdad3305fc88ff80dd93567
adamesalles <adamesalles111@gmail.com>
2021-11-08 07:50:35 -0300

    Adding dynamic page

--- whatsapp_2/urls.py
+++ whatsapp_2/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', wpp2_views.index, name='index'),
     path('contacts/', wpp2_views.contacts, name='contacts'),
     path('profile/', wpp2_views.redirect_to_contacts, name='profile-redirect'),
-    path('profile/<str:param>', wpp2_views.profile, name='profile')
+    path('profile/<str:param>', wpp2_views.profile, name='profile'),
+    path('secret/', wpp2_views.secret, name='secret')
 ]
