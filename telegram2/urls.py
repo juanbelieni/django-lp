14b6a3386572abdf42255cf5841f93f82750f3eb
edilton-bs <ediltonbs5@gmail.com>
2021-11-08 08:37:57 -0300

    aaa

--- telegram2/urls.py
+++ telegram2/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', tlg2_views.index, name="index"),
     path('introducao/', tlg2_views.introducao, name='introducao'),
     path('post/', tlg2_views.redireciona_inicio, name='redirect-inicio'),
-    path('post/<str:text>', tlg2_views.profile, name='postagem')
+    path('post/<str:text>', tlg2_views.post, name='postagem'),
+    path('visualizacao/', tlg2_views.secret, name='vizualizacao')
 ]
