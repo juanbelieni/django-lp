1e80aad3028d738b5fdaf396187373b6078539f8
GustavoSanches55 <gsanchescosta@yahoo.com.br>
2021-11-09 10:45:16 -0300

    Adicionando passos que faltavam

--- instagram2/views.py
+++ instagram2/views.py
@@ -1,6 +1,5 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 def index(request):
@@ -8,3 +7,22 @@ def index(request):
     
 def perfil(request):
     return render(request, "instagram2/index.html")
+
+def profile(request, param):
+    return HttpResponse(f"<h1> Nome: {param} <h2>")
+
+def redirect_to_perfil(request):
+    url = reverse("perfil")
+    return HttpResponseRedirect(url)
+
+def tiorafa(request):
+    lista_de_gostos = ["Cowboy Bebop", "Cavaleiros do Zodiaco", "python", "engenharia"]
+
+    context = {
+        "pfp" : "Imagem do dedão",
+        "name": "Tio Rafa",
+        "user": "@TioR4fa",
+        "beloved": "Cowboy Bebop",
+        "gostos": lista_de_gostos
+    }
+    return render(request, 'instagram2/tioRafa.html', context)
